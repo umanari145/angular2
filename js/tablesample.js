ae35634947771f02fff20e6cd000d4f616c542c1
Matsu <matsumoto@donow.jp>
2017-03-22 18:48:09 +0900

    pulldownの追加

--- js/tablesample.js
+++ js/tablesample.js
@@ -1,7 +1,19 @@
 var app = angular.module("app", []);
 app.controller("tableCtrl", ['$scope', function ( $scope ) {
 
-    $scope.lines = [];
+
+    var pulldownHash  =[
+            {value:'10', label: '未処理'},
+            {value:'20', label: '処理中'},
+            {value:'30', label: '処理済'}
+        ];
+
+    $scope.productPulldownStatus = pulldownHash;
+
+    $scope.lines = [
+        {product_status: '30', name: 'サンプル商品'},
+        {product_status: '20', name: 'サンプル商品2'}
+    ];
 
     $scope.addLine = function () {$scope.lines.push(createExpensesLine());};
 
