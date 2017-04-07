03adb00ac5179236c9681607193af0b23689c429
Matsu <matsumoto@donow.jp>
2017-04-07 06:08:28 +0000

    directiveにクラスのセット

--- js/simple_directive.js
+++ js/simple_directive.js
@@ -9,10 +9,20 @@ app.directive("test", function () {
 }).directive("test2", function () {
     return {
         //Aは属性
-        //テンプレートとして"<div>これはテストだよ！</div>"というテンプレートを使え！
+        //htmlの属性の1つとして付与され、関数をreturnします。
+        //おそらく最も使うパターン
         restrict: "A",
         link: function() {
             alert("テストですが何か?");
+        }
+    };
+}).directive("test3", function () {
+    return {
+        //Cはクラス
+        //その名の通りclassに対して付与されます。
+        restrict: "C",
+        link: function() {
+            alert("クラスについていればOKです。");
         }
     };
 });
