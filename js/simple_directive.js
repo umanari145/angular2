1010c362261bcfd8c1dfb2587616248b1d4d0577
Matsu <matsumoto@donow.jp>
2017-04-07 05:55:36 +0000

    directiveに関数のセット

--- js/simple_directive.js
+++ js/simple_directive.js
@@ -1,8 +1,18 @@
 var app = angular.module("myApp", []);
-
 app.directive("test", function () {
     return {
+        //"test"という名前の要素("E")を持つDOMは
+        //テンプレートとして"<div>これはテストだよ！</div>"というテンプレートを使うという意味
         restrict: "E",
         template: "<div>これはテストだよ！</div>"
     }
-})
+}).directive("test2", function () {
+    return {
+        //Aは属性
+        //テンプレートとして"<div>これはテストだよ！</div>"というテンプレートを使え！
+        restrict: "A",
+        link: function() {
+            alert("テストですが何か?");
+        }
+    };
+});
