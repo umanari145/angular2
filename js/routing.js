024ef7d6610f198525d59e8f59e1c38ea0772af2
Matsu <matsumoto@donow.jp>
2017-05-26 11:30:11 +0900

    queryメソッドでデータを正確に取得

--- js/routing.js
+++ js/routing.js
@@ -1,24 +1,35 @@
-var app = angular.module("myApp", ["ngRoute"]);
+var app = angular.module("myApp", ["ngRoute","ngResource"]);
 app.config(function($routeProvider, $locationProvider) {
     //この指定がないとAngularの1.6以降はroutingがうごかない
     $locationProvider.hashPrefix('');
-
     $routeProvider
     .when("/", {
         templateUrl : "main.html",
         controller : "mainCtrl"
     })
-    .when("/red", {
-        templateUrl : "red.html",
-        controller : "redCtrl"
+    .when("/items", {
+        templateUrl : "items.html",
+        controller : "itemCtrl"
     }).when("/blue", {
         templateUrl : "blue.html",
         controller : "blueCtrl"
     });
 }).controller("mainCtrl", function ($scope) {
     $scope.msg = "color ";
-}).controller("redCtrl", function ($scope) {
-    $scope.msg = "color is red";
+}).controller("itemCtrl", function ($scope, $resource) {
+
+    var rest = $resource("http://192.168.1.28/angular/routing/data.json");
+
+    rest.query(function(data, headers){
+
+        $scope.users = data;
+        console.log("success");
+
+      },function(err) {
+        $scope.msg = "サーバーレスポンスエラー：" + err.statusText;
+      }
+    );
+
 }).controller("blueCtrl",function($scope){
     $scope.msg = "color is blue";
 
